45a9feb0a8329908748568b6fcde09b3f33a1656
dihonestudante <ronneycosta72@gmail.com>
2024-09-13 12:33:10 -0300

    ful commit

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 @app.route("/")
-def home():
-    return render_template('home.html')
+def index():
+  return render_template("login.html")
 
-@app.route('/cliente')
-def cliente():
-    return render_template('cliente.html')
-
-@app.route('/cliente/<cliente_id>')
-def cliente_id(cliente_id):
-    return render_template('cliente_id.html',cliente_id=cliente_id)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
