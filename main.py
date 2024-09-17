aa378b7642c87ea2de0aad8e63e7431b27703c93
dihone <104149290+dihonestudante@users.noreply.github.com>
2024-09-17 20:15:53 -0300

    Update main.py
    atualizando commit

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect, request
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY']= "DIHONE"
 @app.route("/")
-def index():
-  return render_template("login.html")
+def home():
+  return render_template('login.html')
 
-app.run(debug=True)
+@app.route('/login',methods=['POST'])
+def login():
+  nome = request.form.get('nome')
+  senha =  request.form.get('senha')
+  
+  if nome == 'dihone' and senha=='123':
+    return render_template('usuario.html')
+  else:
+    return redirect('/')
+
+if __name__ == "__main__":
+  app.run(debug=True)
